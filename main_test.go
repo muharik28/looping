5a99e6140bc5441a213d6684222e47acb82c19fb
muharik28 <ahmadmuharik@gmail.com>
2026-09-26 10:34:54 +0700

    feat: Unit test diamond pattern

--- main_test.go
+++ main_test.go
@@ -28,3 +28,20 @@ func TestDescendingTrianglePatternNoTrailingNewline(t *testing.T) {
 		t.Fatalf("Generate() = %q, want %q", got, want)
 	}
 }
+
+/*
+	Result for 5 stars
+*/
+//   *    *    *    *    *
+//  ***  ***  ***  ***  ***
+// *************************
+//  ***  ***  ***  ***  ***
+//   *    *    *    *    *
+func TestDiamondPattern(t *testing.T) {
+	got := DiamondPattern{}.Generate(5)
+	want := "  *    *    *    *    *  \n ***  ***  ***  ***  *** \n*************************\n ***  ***  ***  ***  *** \n  *    *    *    *    *  "
+
+	if got != want {
+		t.Fatalf("Generate() = %q, want %q", got, want)
+	}
+}
